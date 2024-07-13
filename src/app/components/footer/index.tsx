f9450672bbe476d2a1a6e3ac1b866c76f84eb279
T-N9 <thethoriumtension@gmail.com>
2024-07-13 10:32:22 +0700

    [Footer] make links accesible

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -30,43 +30,43 @@ const Footer = () => {
                         </p>
 
                         <div className='md:flex justify-start items-center hidden gap-3 mt-5'>
-                            <a href='mailto:info@venuerific.com'>
+                            <a href='mailto:info@venuerific.com' aria-label='Send Mail'>
                                 <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-gray-300'>
                                     <MailIcon />
                                 </span>
                             </a>
-                            <a href='https://www.facebook.com/Venuerific' target='_blank' rel="noreferrer">
+                            <a href='https://www.facebook.com/Venuerific' aria-label='Follow on Facebook' target='_blank' rel="noreferrer">
                                 <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-gray-300'>
                                     <FacebookIcon />
                                 </span>
                             </a>
 
-                            <a href='https://www.instagram.com/venuerificofficial' target='_blank' rel="noreferrer">
+                            <a href='https://www.instagram.com/venuerificofficial' aria-label='Follow on Instagram' target='_blank' rel="noreferrer">
                                 <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-gray-300'>
                                     <InstaIcon />
                                 </span>
                             </a>
 
-                            <a href='https://www.youtube.com/@VenuerificProduction' target='_blank' rel="noreferrer">
+                            <a href='https://www.youtube.com/@VenuerificProduction' aria-label='Follow on YouTube' target='_blank' rel="noreferrer">
                                 <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-gray-300'>
                                     <YouTubeIcon />
                                 </span>
                             </a>
 
 
-                            <a href='#' target='_blank' rel="noreferrer">
+                            <a href='#' target='_blank' aria-label='Follow on Pinterest' rel="noreferrer">
                                 <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-gray-300'>
                                     <PinterestIcon />
                                 </span>
                             </a>
 
-                            <a href='https://sg.linkedin.com/company/venuerific' target='_blank' rel="noreferrer">
+                            <a href='https://sg.linkedin.com/company/venuerific' aria-label='Follow on LinkedIn' target='_blank' rel="noreferrer">
                                 <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-gray-300'>
                                     <LinkedIcon />
                                 </span>
                             </a>
 
-                            <a href='https://www.venuerific.com/blog' target='_blank' rel="noreferrer">
+                            <a href='https://www.venuerific.com/blog' aria-label='Read our blogs' target='_blank' rel="noreferrer">
                                 <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-gray-300'>
                                     <BloggerIcon />
                                 </span>
@@ -77,10 +77,10 @@ const Footer = () => {
                     <div className='md:hidden flex-1 text-center md:text-left'>
                             <h1 className='font-bold text-sm text-gray-800 md:text-inherit md:text-2xl mb-3'>Payment Partners</h1>
                             <ul className='flex items-center md:items-start md:flex-col flex-wrap md:flex-nowrap justify-center gap-8 text-sm'>
-                                <li><a href="#"><Image src="/assets/visa.png" width={66} height={20} alt="Visa Payment"/></a> </li>
-                                <li><a href="#"><Image src="/assets/mastercard.png" width={38} height={24} alt="MasterCard Payment"/></a></li>
-                                <li><a href="#"><Image src="/assets/paynow.png" width={54} height={34} alt="PayNow Payment"/></a></li>
-                                <li><a href="#"><Image src="/assets/duitnow.png" width={46} height={34} alt="DuitNow Payment"/></a></li>
+                                <li><a href="#" aria-label='Payment'><Image src="/assets/visa.png" width={66} height={20} alt="Visa Payment"/></a> </li>
+                                <li><a href="#" aria-label='Payment'><Image src="/assets/mastercard.png" width={38} height={24} alt="MasterCard Payment"/></a></li>
+                                <li><a href="#" aria-label='Payment'><Image src="/assets/paynow.png" width={54} height={34} alt="PayNow Payment"/></a></li>
+                                <li><a href="#" aria-label='Payment'><Image src="/assets/duitnow.png" width={46} height={34} alt="DuitNow Payment"/></a></li>
 
                             </ul>
                         </div>
@@ -89,11 +89,11 @@ const Footer = () => {
                         <div className='flex-1 text-center md:text-left'>
                             <h1 className='font-bold text-sm text-gray-800 md:text-inherit md:text-2xl mb-3'>Menu</h1>
                             <ul className='flex md:items-start md:flex-col flex-wrap md:flex-nowrap justify-center gap-4 text-sm'>
-                                <li><a href="#">Blog</a> </li>
-                                <li><a href="#">Videos</a></li>
-                                <li><a href="#">Event</a></li>
-                                <li><a href="#">Press</a></li>
-                                <li><a href="#">Partners</a></li>
+                                <li><a href="#" aria-label='Follow Sitemap'>Blog</a> </li>
+                                <li><a href="#" aria-label='Follow Sitemap'>Videos</a></li>
+                                <li><a href="#" aria-label='Follow Sitemap'>Event</a></li>
+                                <li><a href="#" aria-label='Follow Sitemap'>Press</a></li>
+                                <li><a href="#" aria-label='Follow Sitemap'>Partners</a></li>
                                 <li className='md:hidden'><a href="#">Ads and Video</a></li>
                             </ul>
                         </div>
@@ -101,20 +101,20 @@ const Footer = () => {
                         <div className='flex-1 text-center md:text-left'>
                             <h1 className='font-bold text-sm text-gray-800 md:text-inherit md:text-2xl mb-3'>Information</h1>
                             <ul className='flex md:items-start md:flex-col flex-wrap md:flex-nowrap justify-center gap-4 text-sm'>
-                                <li><a href="#">How It Works</a> </li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">FAQ</a></li>
-                                <li><a href="#">Terms and Conditions</a></li>
-                                <li><a href="#">Privacy</a></li>
+                                <li><a href="#" aria-label='Follow sitemap'>How It Works</a> </li>
+                                <li><a href="#" aria-label='Follow sitemap'>About</a></li>
+                                <li><a href="#" aria-label='Follow sitemap'>FAQ</a></li>
+                                <li><a href="#" aria-label='Follow sitemap'>Terms and Conditions</a></li>
+                                <li><a href="#" aria-label='Follow sitemap'>Privacy</a></li>
                             </ul>
                         </div>
 
                         <div className='flex-1 text-center md:text-left'>
                             <h1 className='font-bold text-sm text-gray-800 md:text-inherit md:text-2xl mb-3'>Office</h1>
                             <ul className='flex flex-wrap gap-4 text-sm'>
-                                <li><a className='flex items-start gap-2' href="tel:+6569558770"><PhoneIcon /> +65 6955 8770</a> </li>
-                                <li><a className='flex items-start gap-2' href="mailto:info@venuerific.com"><MailSmIcon />info@venuerific.com</a></li>
-                                <li><a className='flex items-start gap-2' href="#"><span className='w-4 h-4'>
+                                <li><a aria-label='Dial phone' className='flex items-start gap-2' href="tel:+6569558770"><PhoneIcon /> +65 6955 8770</a> </li>
+                                <li><a aria-label='Mail to Venuerific' className='flex items-start gap-2' href="mailto:info@venuerific.com"><MailSmIcon />info@venuerific.com</a></li>
+                                <li><a aria-label='Address' className='flex items-start gap-2' href="#"><span className='w-4 h-4'>
                                     <LocationSmIcon />
                                 </span> 9 Raffles Pl, Republic Plaza, Singapore 048619</a></li>
 
@@ -125,30 +125,30 @@ const Footer = () => {
                             <h1 className='font-bold text-sm text-gray-800 md:text-inherit md:text-2xl mb-3'>Follow Us</h1>
 
                             <div className='md:hidden justify-center items-center flex gap-3 mt-5'>
-                                <a href='https://www.youtube.com/@VenuerificProduction' target='_blank' rel="noreferrer">
+                                <a href='https://www.youtube.com/@VenuerificProduction' target='_blank' rel="noreferrer" aria-label='Follow on YouTube'>
                                     <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-primary'>
                                         <YouTubeIcon fillOpacity={1} fill='#3373EF' />
                                     </span>
                                 </a>
-                                <a href='https://www.facebook.com/Venuerific' target='_blank' rel="noreferrer">
+                                <a href='https://www.facebook.com/Venuerific' target='_blank' rel="noreferrer" aria-label='Follow on Facebook'>
                                     <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-primary'>
                                         <FacebookIcon />
                                     </span>
                                 </a>
-                                <a href='mailto:info@venuerific.com'>
+                                <a href='mailto:info@venuerific.com' aria-label='Mail to Venuerific'>
                                     <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-primary'>
                                         <MailIcon />
                                     </span>
                                 </a>
 
 
-                                <a href='https://sg.linkedin.com/company/venuerific' target='_blank' rel="noreferrer">
+                                <a href='https://sg.linkedin.com/company/venuerific' target='_blank' rel="noreferrer" aria-label='Follow on LinkedIn'>
                                     <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-primary'>
                                         <LinkedIcon />
                                     </span>
                                 </a>
 
-                                <a href='https://www.instagram.com/venuerificofficial' target='_blank' rel="noreferrer">
+                                <a href='https://www.instagram.com/venuerificofficial' target='_blank' rel="noreferrer" aria-label='Follow on Instagram'>
                                     <span className='w-8 h-8 inline-flex justify-center items-center rounded-full bg-primary'>
                                         <InstaIcon />
                                     </span>
